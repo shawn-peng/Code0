f6fa561d4f1ae0fdba8ca5b2efbe36def51fba12
U-SHAWN-LAPTOP\Shawn <shawn_pys@yahoo.com>
2015-12-26 03:01:57 -0500

    [scripts rename] rule read

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. ./path
+
 function parse_argument() {
 	while [[ $# > 1 ]]
 	do
@@ -17,8 +19,25 @@ function parse_argument() {
 	echo RULE_FILE = "$RULE_FILE"
 }
 
+RULE_FILE="rename.rules"
+
 parse_argument "$@"
 
-if -n "${RULE_FILE}"
+if [[ ! -f "${RULE_FILE}" ]]
 then
-	
+	echo "rule file not exists."
+	exit
+fi
+
+sed -ie "s/\r//g" ${RULE_FILE}
+
+{
+	read from to 
+	while [[ $? == 0 ]]
+	do
+		echo Replacing \"$from\" with \"$to\"
+
+
+		read from to
+	done
+} < "$RULE_FILE"
